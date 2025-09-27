051ed61cb1bb6c2832569b0ba54a6e7b12d9bc2c
fahs3 <faheem.saeed15@gmail.com>
2025-09-26 20:58:57 -0700

    Updates to readme, etc.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# **Assignment 1: "Hello World"**
+# **Assignment 2: "Area of Rectangle Calculator"**
 **Author:** Faheem Saeed  
 **Student Number:** 18133158  
 **Course:** MECH 524  
@@ -8,31 +8,32 @@ The purpose of this readme is to indicate the required dependencies, how to copy
 
 ## **Referenced Material**
 ChatGPT used to understand how to format a README.md using # and * for italicization, bolding, different header classes, and how to put code in a readme using `your code here`.
-Assignment Guidelines.pdf used to understand how to format the readme, v
+Assignment Guidelines.pdf used to understand how to format the readme.
 
 ## **Codebase Purpose**
-The purpose of this codebase is to create a simple executable program in python which prints a given statement, in this case, hello world, to the terminal and stays active there until termination by the user.
+The purpose of this codebase is to create a simple executable program in python which serves to calculate the area of the two rectangles given doubles for length and width information. It returns a bool variable `0` for if the areas of the rectangle are the same and `1` if not. It has necessary error handling functionality to indicate if a negative value, a 0, or a non floating point variable has been provided as a rectangle parameter and throws an AssertionError. If the variables provided
+are valid, it calculates the area of both rectangles and compares if they are the same. Due to floating point errors as it is difficult to represent a base 10 decimal as a base 2, both areas are compared to determine if they are within a defined relative tolerance. To do this and make the program robust and buildable upon for the future, a class and appropriate methods have been setup, as is common in object oriented programming (OOP).
 
 ## **License**
 This work is guarded by the MIT No Attribution License, last updated on May 15, 2020.
 
 ## **Change Log**
-- <1.0> Initial codebase release on 2025-09-17. Simple executable program to print a statement "Hello World" on terminal.
+- <1.0> Initial codebase release on 2025-09-24. Simple executable program to calculate area of 2 rectangles, determine
 
 ## **Features**
 - Inputted statement in main.py printed in terminal when executable is run, and waits for user
 
 ## **Dependencies & Frameworks**
 pyinstaller==6.0+ (below dependencies for pyinstaller)
+    [altgraph==0.17.4
+    packaging==25.0
+    pefile==2023.2.7
+    pyinstaller==6.16.0
+    pyinstaller-hooks-contrib==2025.8
+    pywin32-ctypes==0.2.3
+    setuptools==80.9.0]
 
-[altgraph==0.17.4
-packaging==25.0
-pefile==2023.2.7
-pyinstaller==6.16.0
-pyinstaller-hooks-contrib==2025.8
-pywin32-ctypes==0.2.3
-setuptools==80.9.0
-keyboard==0.13.5]
+The math and sys libraries are included in Python, and so they are not considered as external dependencies.
 
 ## **Installation**
 1. Install Python 3.1+, pyinstaller releases support Python 3.7 - 3.12
@@ -40,12 +41,12 @@ keyboard==0.13.5]
 2. Install an IDE like VSCode
 
 3. Clone the repository via bash
-    git clone https://github.com/fsaeed2/M524_HW1.git  
-    cd M524_HW1  
+    git clone https://github.com/fsaeed2/M524_HW2.git  
+    cd M524_HW2  
 
 4. Create and activate virtual environment, installing all dependencies:
 
-`python -m venv M524`  
+`python -m venv M524`
 `M524\Scripts\activate`  
 
 5. Install dependencies found in requirements.txt
@@ -53,6 +54,18 @@ keyboard==0.13.5]
 `pip install -r requirements.txt`  
 
 6. If future dependencies added to the project, use `pip freeze > requirements.txt` to update dependency file.
+
+## **Debugging Instructions**
+Using VSCode or similar, after setting up virtual environment and installing dependencies, the developer can
+utilize the debugger to step through the code. This can be done by setting breakpoints by clicking the left of the
+row number in main.py, and setting red breakpoints where outputs can be analyzed and code can be stepped through
+line by line. To run the debugger, use the keybinding `CTRL+F5` or click the downwards chevron next to the run icon and
+press `Python Debugger: Debug Python File.`
+
+## **Tested Platform/ Versions**
+1. Operating System: Windows 11
+2. Hardware: PC
+3. Python Version: Python 3.1+. pyinstaller releases support Python 3.7 - 3.12.
 
 ## **Compiling & Execution**
 1. Navigate to project folder in your terminal (bash)
--- main.py
+++ main.py
@@ -1,78 +1,158 @@
 # ************************************************************ HEADER ************************************************************ #
-# - Assignment 1: Hello World
+# - Assignment 2: Area of Rectangle Calculator
 #
 # - Name: Faheem Saeed
 # - Student ID: 18133158
-# - File: main.py
-# - Purpose: Contains the main function, which serves to display a hello world on a terminal.
-# - Description: Main program source code, which uses a class called hello_world. The class hello_world has 2 functions.
-# The first function __init__(self, statement) initializes an object which has a label called statement in it. It also sets the input statement
-# to the object. The second function called greet(self) utilizes the created object to print the label statement in it. 
-# To compile this code, utilize pyinstaller as indicated in the README.md. To use this code for displaying another statement on the terminal, 
-# alter the global variable <statement>.
+# - File Name: main.py
+
+# - Purpose: Contains the main method, which serves to calculate the area of the two rectangles given doubles for length and width information for
+# the rectangles, then compares them to check if they are the same.
+
+# - Description: Main program source code, which uses a class called rectangle_area_class. The class has 4 methods.
+# The first method __init__(self, statement) initializes an object which has a attributes for the lengths and widths of the two rectangles. 
+# The second method called eval_variables(self) utilizes the inputted attributes from the main function and checks if they are floats and positive non-zero
+# numbers. It returns a list of booleans indicating if the parameters provided for both rectangles are correct for providing a positive real non-zero area.
+# The third method area_calculator(self) calculates the areas of the two rectangles and puts them in a list, which is returned.
+# The fourth method eval_area_similarity(self) compares the two areas in the list and verifies that they are equivalent to each other within a certain relative
+# tolerance, provided to account for floating point base 2 errors in trying to quantify a base 10 decimal number.
+# To compile this code, utilize pyinstaller as indicated in the README.md. To use this code for calculating the areas of other rectangles, alther the variables provided
+# in the main function. To pack the script, follow the README.md instructions to use pyinstaller.
 
 # ************************************************************ REFERENCED MATERIAL *********************************************** #
 
-#ChatGPT used for instructions on how to setup the class, and definitions related to __init__, {} usage, etc.
-#I learnt how to use pyinstaller from ChatGPT to bundle the application and its dependencies into an exe file.
-#Used ChatGPT for code review - to see if following PEP 8 coding stylization for Python, and determine differences to judge if acceptable or not.
+#ChatGPT used for reference on how to use typing notations to indicated expected variable type for classes in Python.
+#ChatGPT used to understand how to utilize math.isclose to check if both areas similar within desired tolerance.
+#ChatGPT used for code review - to see if following PEP 8 coding stylization for Python, and determine differences to judge if acceptable or not.
+#Style followed is not exactly PEP-8, as Professor Christoph allows following own convention as long as consistency is shown.
 #Referenced provided header example to setup header.
 
 # ************************************************************ DEPENDENCIES ****************************************************** #
-
+import sys
+import math
 # pyinstaller==6.0+ (below dependencies for pyinstaller)
-
-#     [altgraph==0.17.4
-#     packaging==25.0
-#     pefile==2023.2.7
-#     pyinstaller==6.16.0
-#     pyinstaller-hooks-contrib==2025.8
-#     pywin32-ctypes==0.2.3
-#     setuptools==80.9.0
-#     keyboard==0.13.5]
+# altgraph==0.17.4
+# packaging==25.0
+# pefile==2023.2.7
+# pyinstaller==6.16.0
+# pyinstaller-hooks-contrib==2025.9
+# pywin32-ctypes==0.2.3
+# setuptools==80.9.0
 
 # ************************************************************ GLOBAL VARIABLES ************************************************** #
 
-# statement = "statement text here"
+# No global variables present, all variables stored in main() function.
 
-# ************************************************************ FUNCTION & CLASS DEFINITIONS ************************************** #
+# ************************************************************ MAIN CLASS & METHOD DEFINITIONS ************************************** #
 
-# Class: hello_world
-# Arguments: (statement: str)
-#       The string statement that you want to print onto the console - in this case hello world.
+# Class: rectangle_area_class
+# Arguments: (self, length1, length2, length3, length4)
+#       The variables that you want to set to become the length and widths of the two triangles (double)
 #       Constructor __init__ runs automatically when you create an object from the class.
-#       It stores the statement inputted into a label called statement in the object using self.statement.
+#       It stores the variables inputted in the object using self.width1, self.width2, self.length1, self.length2
 
-class hello_world:
+class rectangle_area_class:
 
-    # Function: __init__
-    # Arguments: self, statement
+    # Method: __init__
+    # Arguments: self
     # Returns: nothing
     # Logic: creates constructor which runs when you make an object from the class
-    # It sets data up in the object, putting the statement text inside the object for later use.
+    # It sets data up in the object, putting the lengths and widths entered into attributes in the object for later use.
 
-    def __init__(self, statement: str) -> None:
-        #self refers to the object being created, which has a label called statement inside it
-        self.statement = statement
+    def __init__(self, length1: float, length2: float, width1: float, width2: float) -> None:
+        #self refers to the object being created, which initializes attributes called width1, width2, length1, length2 inside it and stores given values.
+        self.length1 = length1
+        self.length2 = length2
+        self.width1 = width1
+        self.width2 = width2
 
-    # Function: greet(self)
+    # Method: eval_variables(self)
     # Arguments: self
-    # Returns: printed statement
-    # Logic: function inside the class which is meant to allow the object to do an action
-    # when you call the greet function with the object, it will print out the statement
+    # Returns: variable_bool_list (list): a list of 4 booleans which evaluate to TRUE if the parameters provided are positive greater than zero floats
+    # Logic: method inside the class which is meant to evaluate the attributes length1, length2, width1, width2 inside 
+    # Evaluates each attribute to ensure that a float (C double) has been provided which is positive and greater than 0.
 
-    def greet(self) -> None:
-        print(f"{self.statement}")
+    def eval_variables(self) -> list[bool, bool, bool, bool]:
+        
+        length1_bool = isinstance(self.length1, float) and self.length1 > 0
+        length2_bool = isinstance(self.length2, float) and self.length2 > 0
+        width1_bool = isinstance(self.width1, float) and self.width1 > 0
+        width2_bool = isinstance(self.width2, float) and self.width1 > 0
 
-#storing string hello world onto statement global variable
-statement = "I would like to learn how to implement computer vision and machine learning for robotics, along with how to develop a basic complete software package for robots"
+        variable_bool_list = [length1_bool, length2_bool, width1_bool, width2_bool]
+        return variable_bool_list
+    
+    # Method: area_calculator(self)
+    # Arguments: self
+    # Returns: area_rect_list (list): a list of 2 floating point numbers which represent the areas of the two rectangles for the parameters provided.
+    # Logic: method inside the class which is meant to calculate the area of two rectangles given the parameters for it and store
+    # them in a list named area_rect_list.
 
-#Creating a new object using the class hello world with the statement hello world inputted
-#Uses input instead to wait for user to press a button then exits.
-statement_obj = hello_world(statement)
-statement_obj.greet()
+    def area_calculator(self) -> list[float, float]:
 
-# #Waits for a key to be pressed before exiting application.
-# print("Press any button to exit...")
-# keyboard.read_event()
+         area_rect_list = [self.width1*self.length1, self.width2*self.length2]
+         self.area_rect_list = area_rect_list
+         return area_rect_list
+    
+    # Method: eval_area_similarity(self)
+    # Arguments: self
+    # Returns: area_similarity_bool (bool): A boolean variables which evaluates to TRUE if both areas are within the relative tolerance, returns FALSE if not.
+    # Logic: method inside the class which evaluates the areas in the list of the object area_rect_list. Uses math.isclose to check if
+    # both areas are close to each other within the relative tolerance indicated. Returns area_similarity_bool as TRUE/ FALSE.
+    
+    def eval_area_similarity(self) -> bool:
+         area_similarity_bool = math.isclose(self.area_rect_list[0], self.area_rect_list[1], rel_tol=1e-09)
+         return area_similarity_bool
+    
+# ************************************************************ MAIN FUNCTION ************************************************** #
 
+# Function: main()
+# Arguments: none
+# Returns: nothing, only shows the parameters on the terminal window.
+# Shows: terminal window with TRUE/FALSE for each parameter, indicating if they are a floating point number > 0. If false shown for one or more
+# Variable, exits and raises AssertionError. If all variables are correct, will then calculate the area, show it on the terminal, and then indicate if
+# The areas are same within a tolerance setup in the above class.
+def main():
+
+    # Creating float (python float is implemented as a C double) variables and 
+    # using them to store the lengths and widths as Python does not have a double data type.
+    # Python number data types are: int, long, float, complex.
+    length1: float = float(0.1)
+    width1: float = float(0.6)
+    length2: float = float(1)
+    width2: float = float(0.06)
+
+    # Creating a new object using the class rectangle_area_class with the variables inputted.
+    variable_obj = rectangle_area_class(length1, length2, width1, width2)
+    variable_bool_list = variable_obj.eval_variables()
+
+    print(f"Length 1 dimension validity: {variable_bool_list[0]}\n",
+            f"Length 2 dimension validity: {variable_bool_list[1]}\n",
+            f"Width 1 dimension validity: {variable_bool_list[2]}\n",
+            f"Width 2 dimension validity: {variable_bool_list[3]}\n",)
+
+    # Assert statement checks if all the booleans variables in the variable_bool_list are true - if assertation fails
+    # Python raises assertation error giving the error (written next to the assert statement), prints it, then exits the code.
+    try:
+        assert all(variable_bool_list), "One or more rectangular dimensions are not positive floats"
+    except AssertionError as e:
+        print(e)
+        sys.exit(1)
+
+    area_rect_list = variable_obj.area_calculator()
+    area_similarity_bool = variable_obj.eval_area_similarity()
+
+    print(f"The area of the first rectangle is: {area_rect_list[0]}")
+    print(f"The area of the second rectangle is: {area_rect_list[1]}")
+    print(f"The areas have been evaluated to be the same: {area_similarity_bool}")
+
+# ************************************************************ SCRIPT TO RUN MAIN FUNCTION ************************************************** #
+
+# Running the main function if this program is directly excecuted.
+if __name__ == "__main__":
+    main()
+
+    #Code for requiring enter to exit to keep terminal window open, as suggested by TA for prior assignment.
+    try:
+            input("\nPress Enter to exit...")
+    except EOFError:
+        pass
